954e38501ece7a6015b5dab5ed098656df8d5b65
Russell Gordon <rgordon@lcs.on.ca>
2020-05-05 07:57:40 -0400

    Now have two instances of "Perlin waves", in an array.

--- Animation/PerlinWave.swift
+++ Animation/PerlinWave.swift
@@ -9,29 +9,31 @@
 import Foundation
 import CanvasGraphics
 
-struct PerlinWave {
+class PerlinWave {
     
     // MARK: Properties
     var c: Canvas
     let generator: PerlinGenerator
     var increment: Double
     var color: Color
+    var verticalSpacePosition: Double
 
     // MARK: Initializer
-    init(canvasToDrawUpon: Canvas, increment: Double = 0.001, color: Color = .black) {
+    init(canvasToDrawUpon: Canvas, increment: Double = 0.001, color: Color = .black, verticalSpacePosition: Double = 0) {
         
         self.c = canvasToDrawUpon
         self.generator = PerlinGenerator()
         self.increment = increment
         self.color = color
+        self.verticalSpacePosition = verticalSpacePosition
         
     }
     
     // MARK: Methods
-    mutating func update() {
+    func update() {
         
         // Generate noise, a random value between 0 and 1, where each successive value is close to the prior value
-        let naturalNoise = generator.perlinNoise(x: Double(c.frameCount) * increment)
+        let naturalNoise = generator.perlinNoise(x: Double(c.frameCount) * increment, y: verticalSpacePosition)
         print(naturalNoise)
         
         // Map to a height relative to the screen
--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -7,8 +7,8 @@ class Sketch : NSObject {
     //       Therefore, the line immediately below must always be present.
     let canvas : Canvas
     
-    // Create a "Perlin wave"
-    var wave: PerlinWave
+    // Create two "Perlin waves"
+    var waves: [PerlinWave] = []
     
     // This function runs once
     override init() {
@@ -16,16 +16,26 @@ class Sketch : NSObject {
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
         
-        // Set up this Perlin wave
-        wave = PerlinWave(canvasToDrawUpon: canvas, increment: 0.001, color: .black)
-        
+        // Set up a Perlin wave
+        waves.append(PerlinWave(canvasToDrawUpon: canvas,
+                                increment: 0.001,
+                                color: Color(hue: 240, saturation: 80, brightness: 90, alpha: 100),
+                                verticalSpacePosition: 1))
+
+        waves.append(PerlinWave(canvasToDrawUpon: canvas,
+                                increment: 0.001,
+                                color: Color(hue: 60, saturation: 80, brightness: 90, alpha: 100),
+                                verticalSpacePosition: 2))
+
     }
     
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
 
         // Draw this wave
-        wave.update()
+        for wave in waves {
+            wave.update()
+        }
         
     }
         
