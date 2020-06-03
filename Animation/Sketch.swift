264f529d41ed8c431fa0d57124cb93c93500cc45
lcs-JZhang <XZhang@lcs.on.ca>
2020-06-02 20:16:33 -0400

    initial commit

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -21,22 +21,41 @@ class Sketch : NSObject {
                                 increment: 0.001,
                                 color: Color(hue: 240, saturation: 80, brightness: 90, alpha: 100),
                                 verticalSpacePosition: 1))
-
+        
         waves.append(PerlinWave(canvasToDrawUpon: canvas,
                                 increment: 0.001,
                                 color: Color(hue: 60, saturation: 80, brightness: 90, alpha: 100),
                                 verticalSpacePosition: 2))
-
+        
+        waves.append(PerlinWave(canvasToDrawUpon: canvas,
+                                increment: 0.001,
+                                color: Color(hue: 180, saturation: 80, brightness: 90, alpha: 100),
+                                verticalSpacePosition: 3))
+        
+        
     }
     
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
-
+        
         // Draw this wave
         for wave in waves {
             wave.update()
         }
         
-    }
+        let height = Double.random(in:0...Double(canvas.height))
+        print (height)
         
+        
+        //        //print the current frame
+        //       print(canvas.frameCount)
+        
+        
+        //       //draw a line
+        //       let from = Point(x: canvas.frameCount, y:0)
+        //     let to = Point(x: Double(canvas.frameCount), y: height)
+        //     canvas.drawLine(from: from, to: to)
+        
+    }
+    
 }
