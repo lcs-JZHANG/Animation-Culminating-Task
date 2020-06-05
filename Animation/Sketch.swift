04878069127d186a2fa7f468cb85f8648f49e984
lcs-JZhang <XZhang@lcs.on.ca>
2020-06-05 16:16:35 -0400

    debug and improve

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,71 +8,71 @@ class Sketch : NSObject {
     
     let canvas : Canvas
     
-      // position
-        var x : Int
-        var x1 : Int
+    // position
+    var x : Int
+    var x1 : Int
+    
+    let black = Color(hue: 0, saturation: 100, brightness: 0, alpha: 100)
+    let blue = Color(hue: 240, saturation: 100, brightness: 100, alpha: 100)
+    let green = Color(hue: 100, saturation: 100, brightness: 100, alpha: 100)
+    let orange = Color(hue: 35, saturation: 100, brightness: 100, alpha: 100)
+    let purple = Color(hue: 280, saturation: 70, brightness: 60, alpha: 100)
+    
+    var drawShapesWithBorders = false
+    
+    // The function
+    override init() {
         
-        let black = Color(hue: 0, saturation: 100, brightness: 0, alpha: 100)
-        let blue = Color(hue: 240, saturation: 100, brightness: 100, alpha: 100)
-        let green = Color(hue: 100, saturation: 100, brightness: 100, alpha: 100)
-        let orange = Color(hue: 35, saturation: 100, brightness: 100, alpha: 100)
-        let purple = Color(hue: 280, saturation: 70, brightness: 60, alpha: 100)
+        // Position
+        canvas = Canvas(width: 500, height: 500)
         
-        var drawShapesWithBorders = false
+        // Set starting position
+        x = 250
+        x1 = 250
         
-        // The function
-        override init() {
+    }
+    
+    // Making it to repeat
+    func draw() {
+        
+        canvas.drawShapesWithBorders = false
+        
+        //three circles going down
+        for n in 0...2{
             
-            // Position
-            canvas = Canvas(width: 500, height: 500)
+            x += 1
             
-            // Set starting position
-            x = 250
-            x1 = 250
+            // color
+            if n == 0{
+                canvas.fillColor = black
+            } else if n == 1{
+                canvas.fillColor = blue
+            } else if n == 2{
+                canvas.fillColor = purple
+            }
+            
+            // draw ellipse
+            canvas.drawEllipse(at: Point(x: 83 + 166 * n, y: x), width: 50, height: 50)
             
         }
         
-        // Making it to repeat
-        func draw() {
+        
+        //three circles going down
+        for n in 0...2{
             
-            canvas.drawShapesWithBorders = false
+            x += 1
             
-            //three circles going down
-            for n in 0...2{
-                
-                x += 1
-                
-                // color
-                if n == 0{
-                    canvas.fillColor = black
-                } else if n == 1{
-                    canvas.fillColor = blue
-                } else if n == 2{
-                    canvas.fillColor = purple
-                }
-                
-                // draw ellipse
-                canvas.drawEllipse(at: Point(x: 83 + 166 * n, y: x), width: 50, height: 50)
-                
+            // color
+            if n == 0{
+                canvas.fillColor = orange
+            } else if n == 1{
+                canvas.fillColor = black
+            } else if n == 2{
+                canvas.fillColor = green
             }
             
-            
-                      //three circles going down
-                      for n in 0...2{
-                          
-                          x += 1
-                          
-                          // color
-                          if n == 0{
-                              canvas.fillColor = orange
-                          } else if n == 1{
-                              canvas.fillColor = black
-                          } else if n == 2{
-                              canvas.fillColor = green
-                          }
-                          
-                          // draw ellipse
-                          canvas.drawEllipse(at: Point(x: 60 + 100 * n, y: x), width: 50, height: 50)
+            // draw ellipse
+            canvas.drawEllipse(at: Point(x: 150 + 166 * n, y: x), width: 50, height: 50)
             
             
             // go up
@@ -93,25 +93,29 @@ class Sketch : NSObject {
             }
             
             // go up
-                for n in 0...1{
-                    
-                    x1 -= 1
-                    
-                    //colors of it
-                    if n == 0{
-                        canvas.fillColor = purple
-                    } else if n == 1{
-                        canvas.fillColor = green
-                    }
-                    
-                    // Draw ellipse
-                    canvas.drawEllipse(at: Point(x: 100 + 150 * n, y: x1), width: 50, height: 50)
-                    
+            for n in 0...1{
+                
+                x1 -= 1
+                
+                //colors of it
+                if n == 0{
+                    canvas.fillColor = purple
+                } else if n == 1{
+                    canvas.fillColor = green
                 }
+                
+                // Draw ellipse
+                canvas.drawEllipse(at: Point(x: 100 + 150 * n, y: x1), width: 50, height: 50)
+                
+            }
+            
+            
+            
+            
             
         }
         
     }
-
+    
     
 }
